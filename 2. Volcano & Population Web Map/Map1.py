82cb2d58e47edb0c7db4c4d9667ed31b6b53b987
Cazeck <caseykolodziejczyk@gmail.com>
2021-01-22 01:32:05 -0600

    Now pulls coordinates and elevation data from volcanoes.txt and adds them to the map through dynamic pointers

--- 2. Volcano & Population Web Map/Map1.py
+++ 2. Volcano & Population Web Map/Map1.py
@@ -1,6 +1,3 @@
-"""
-Folium makes maps by turning python code into html and javascript
-"""
 import folium
 import pandas
 
@@ -8,17 +5,18 @@ data = pandas.read_csv("Volcanoes.txt")
 
 # Creates base layer map
 map = folium.Map(location=[38.58,-99.09], zoom_start=6, tiles="Stamen Terrain")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 # Can add multiple features within this FeatureGroup such as map markers
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev): # zip iterates through multiple lists simultaneously
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
 
 # Now we can add all of our features to the map at once
 map.add_child(fg)
 
 map.save("Map1.html")
-
-
 
