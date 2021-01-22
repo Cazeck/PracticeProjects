a583462ad244c8c1efa31548e9a7b08b24b37fb5
Cazeck <caseykolodziejczyk@gmail.com>
2021-01-22 04:09:29 -0600

    Made a small personal website using Flask library, HTML, and CSS

---
+++ 0. Sentence Maker/errors.py
@@ -0,0 +1,11 @@
+"""
+Just some small things about reading and handling errors
+
+Two types of errors:
+1) Syntax Errors: Poor formatting, misuse of method, wrong typing, etc
+
+2) Exceptions:
+
+
+"""
+
