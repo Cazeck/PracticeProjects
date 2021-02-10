9a9467da5222b08abf600ad8c5ea0e88be0d81df
Cazeck <caseykolodziejczyk@gmail.com>
2021-02-10 00:01:55 -0600

    Looping through property listings and extracting property details

--- 7. Real Estate Webscraper/century21.py
+++ 7. Real Estate Webscraper/century21.py
@@ -10,5 +10,36 @@ soup = BeautifulSoup(c, "html.parser")
 # Pull each of the propertyRow divs from the HTML
 all = soup.find_all("div", {"class": "propertyRow"})
 
-# Pulling the Price of a property from a row
-print(all[0].find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", ""))
+# Looping through the propertyRows and extracting data
+for item in all:
+    # Price
+    print(item.find("h4", {"class": "propPrice"}).text.replace("\n", "").replace(" ", ""))
+    # Address
+    print(item.find_all("span", {"class": "propAddressCollapse"})[0].text)
+    # City
+    print(item.find_all("span", {"class": "propAddressCollapse"})[1].text)
+    # Num of Beds
+    try:
+        print(item.find("span", {"class": "infoBed"}).find("b").text)  # Sometimes this is a None type
+    except:
+        print(None)             # For consistent formatting when we have all of the values
+
+    # Num of SqFt
+    try:
+        print(item.find("span", {"class": "infoSqFt"}).find("b").text)
+    except:
+        print(None)
+
+    # Num of Baths
+    try:
+        print(item.find("span", {"class": "infoValueFullBath"}).find("b").text)
+    except:
+        print(None)
+
+    # Num of Half Baths
+    try:
+        print(item.find("span", {"class": "infoValueHalfBath"}).find("b").text)
+    except:
+        print(None)
+
+    print(" ")
