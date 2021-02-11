a7bf12fb7d47d742903a4c3b0455104ea3f99907
Cazeck <caseykolodziejczyk@gmail.com>
2021-02-11 16:01:41 -0600

    Added candlesticks to the chart as well as some additional stylization

--- 8. Interactive Data Dashboard/stonk.py
+++ 8. Interactive Data Dashboard/stonk.py
@@ -4,7 +4,7 @@ import datetime
 from pandas_datareader import data
 from bokeh.plotting import figure, show, output_file
 
-start = datetime.datetime(2016, 3, 1)
+start = datetime.datetime(2015, 11, 1)
 end = datetime.datetime(2016, 3, 10)
 
 # Grabbing Stock data from Yahoo
@@ -36,19 +36,25 @@ df["Height"] = abs(df.Close - df.Open)
 print(df)
 
 # building figure object
-p = figure(x_axis_type='datetime', width=1000, height=300)
+p = figure(x_axis_type='datetime', width=1000, height=300, sizing_mode="scale_width")
 p.title.text = "Candlestick Chart"
+
+# Chart grid line attributes
+p.grid.grid_line_alpha = 0.3
 
 # 12 Hours in milliseconds
 hours_12 = 12*60*60*1000
 
+# Creating lines for the Candlestick
+p.segment(df.index, df.High, df.index, df.Low, color="black")
+
 # When price closes higher than when it started
 p.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"], hours_12,
-       df.Height[df.Status == "Increase"], fill_color="green", line_color="black")
+       df.Height[df.Status == "Increase"], fill_color="#CCFFFF", line_color="black")
 
 # When price closes lower than when it started
 p.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"], hours_12,
-       df.Height[df.Status == "Decrease"], fill_color="red", line_color="black")
+       df.Height[df.Status == "Decrease"], fill_color="#FF3333", line_color="black")
 
 output_file("CS.html")
 show(p)
