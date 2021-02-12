8a7a9550add5678fda84bd0a7aeebc9bdc8e28e8
Cazeck <caseykolodziejczyk@gmail.com>
2021-02-12 03:33:56 -0600

    Set up PostGreSQL database model, can now store user data into the database.  Website now responds to users input if a email has already been used

--- 9. Database Web App/app.py
+++ 9. Database Web App/app.py
@@ -1,7 +1,20 @@
 # Building an Web Application with Python
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/height_collector'
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
 
 @app.route("/")
 def index():
@@ -14,8 +27,17 @@ def success():
         email = request.form["email_name"]
         height = request.form["height_name"]
         print(email, height)
-        return render_template("success.html")
+        # If an email address has already been used for the survey
+        if db.session.query(Data).filter(Data.email_ == email).count() == 0:
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+
+    return render_template("index.html",
+        text="Seems like we've got something from that email address already!")
 
 if __name__ == '__main__':
+    # Set this to False once you deploy. Good for troubleshooting for now
     app.debug = True
     app.run()
