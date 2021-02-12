b99136cc78c2bfbdf165aa937e91ccef6c02fa94
Cazeck <caseykolodziejczyk@gmail.com>
2021-02-12 01:55:41 -0600

    Fixed CSS issues, python script can now receive the user's input from the website

---
+++ 9. Database Web App/app.py
@@ -0,0 +1,21 @@
+# Building an Web Application with Python
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        # grab the email input
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        print(email, height)
+        return render_template("success.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
--- 9. Database Web App/sonk.py
+++
@@ -1,1 +0,0 @@
-# Building an Web Application with Python
